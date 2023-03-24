8ff0afcc42597e19cb397293c332f64016920f09
Angel Fuentes Pascual <Angeljavier46@gmail.com>
2023-03-24 15:17:27 +0100

    Añadimos un 4 test para que la velocidad no pueda ser inferior a cero

--- test/Coche.java
+++ test/Coche.java
@@ -1,14 +1,17 @@
 public class Coche
 {
-    public int velocidad;
+    int velocidad;
 
     public void acelerar(int aceleracion)
     {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion)
-    {
+    public void decelerar(int deceleracion) {
         velocidad -= deceleracion;
+        if (velocidad < 0)
+        {
+            velocidad = 0;
+        }
     }
 }
